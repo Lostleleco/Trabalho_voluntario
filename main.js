31d8268bb2ce5ec42a1318f1f6e6983dd19ffa03
Alex <alex_soares_oliveira@hotmail.com>
2024-06-08 17:45:24 -0300

    Fix line endings

---
+++ main.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+    $('#meuFormulario').validate({
+      rules: {
+        email: {
+          required: true,
+          minlength: 3
+        },
+        email: {
+          required: true,
+          email: true
+        },
+        mensagem: {
+          required: true,
+          minlength: 10
+        }
+      },
+      messages: {
+        nome: {
+          required: "Por favor, insira seu nome",
+          minlength: "Seu nome deve ter pelo menos 2 caracteres"
+        },
+        email: {
+          required: "Por favor, insira seu email",
+          email: "Por favor, insira um email válido"
+        },
+        mensagem: {
+          required: "Por favor, insira uma mensagem",
+          minlength: "Sua mensagem deve ter pelo menos 10 caracteres"
+        }
+      },
+      submitHandler: function(form) {
+        alert("Formulário enviado com sucesso, Obrigado!");
+        form.submit();
+      }
+    });
+});
